284daa9d3b42d4ff916baac53a684a218980d289
Karthik Th <karthik@kloudlite.io>
2026-08-27 19:19:08 +0530

    Make /{owner} the home of the namespace

--- web/apps/web/src/components/app/home.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "bun:test";
-import { mergeFeeds } from "./home";
-
-test("feeds merge newest first and cap", () => {
-  const a = [{ at: 30 }, { at: 10 }] as never[];
-  const b = [{ at: 20 }] as never[];
-  expect(mergeFeeds([a, b], 2).map((e: { at: number }) => e.at)).toEqual([30, 20]);
-});
