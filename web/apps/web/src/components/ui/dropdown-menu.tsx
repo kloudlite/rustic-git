80f3c458ef1f8d0cd05254f9967936efcbc7fcd4
Karthik Th <karthik@kloudlite.io>
2026-08-28 18:56:32 +0530

    Drop now-unused DialogClose helper and dropdown-menu icon imports

--- web/apps/web/src/components/ui/dropdown-menu.tsx
+++ web/apps/web/src/components/ui/dropdown-menu.tsx
@@ -4,7 +4,6 @@ import * as React from "react"
 import { DropdownMenu as DropdownMenuPrimitive } from "radix-ui"
 
 import { cn } from "@/lib/utils"
-import { CheckIcon, ChevronRightIcon } from "lucide-react"
 
 function DropdownMenu({
   ...props
